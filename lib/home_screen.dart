eb27f74c4d802b2388e46c9b557692e07b1df822
Jean Jonathan <jeanjonathank2@gmail.com>
2023-05-29 10:20:35 +0200

    Implementation de l ecran acceuil

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
-
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( // structure de base pour l'application comprenant app bar et un body
       appBar: AppBar(
-        //leading pour pour spécifier l image comme un widget à afficher à gauche dans AppBar
-        leading: Image.asset('assets/logo.png'),
-        title: Text('progress book'),
+        leading: IconButton( //
+          icon: Icon(Icons.menu), // Icône du menu à gauche
+          onPressed: () {
+            // Action du bouton de menu
+          },
+        ),
         actions: [
+          SizedBox(width: AppBar().preferredSize.height), // Espacement entre l'icône du menu et l'image du logo
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/ocean.png'), // Image du logo au centre
+              ],
+            ),
+          ),
+          SizedBox(width: AppBar().preferredSize.height), // Espacement entre l'image du logo et le bouton des paramètres
           IconButton(
-            icon: Icon(Icons.menu),
+            icon: Icon(Icons.settings), // Icône des paramètres à droite
             onPressed: () {
-              // Action du bouton de menu
+              // Action du bouton de paramètres
             },
           ),
         ],
+        centerTitle: true, // Centrer le contenu de l'AppBar
       ),
-      body: SingleChildScrollView(
-        child: Column(
+
+      body: SingleChildScrollView(//widget pour faire defiler le contenu de la colonne verticale si l espace est inssufisant
+        child: Column(// jouant le role du learLayout vertical
+          3cv ☻
           children: [
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.0),
+              padding: EdgeInsets.symmetric(vertical:5.0),
               child: Text(
                 'IMPROVE YOUR TECHNIQUE',
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
@@ -30,62 +45,33 @@ class HomeScreen extends StatelessWidget {
             Image.asset('assets/progresSurfss.jpg'),
             SizedBox(height: 20.0),
             DisciplineBlock(
-              image: 'assets/surf.jpeg',
-              title: 'Surf',
-              description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            ),
-            DisciplineBlock(
-              image: 'assets/kitesurfs.jpg',
-              title: 'KiteSurf',
-              description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            ),
-            DisciplineBlock(
-              image: 'assets/wingfoils.jpg',
-              title: 'WingFoil',
+              image: 'assets/kitesurfwing.jpg',
+              title: 'Kitesurf        Surf        wingfoil',
               description:
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             ),
             SizedBox(height: 20.0),
-            Row(
+            Row(//pour organiser les enfant en ligne horizontale (linearLayout horizontal)
               children: [
-                Expanded(
-                  child: ProgressBlock(
+                Expanded(//pour prendre autant d'espace disponible que possible
+                  child: DisciplineBlock(
                     image: 'assets/progres.jpg',
-                    title: 'Progress by chapter and levels',
+                    title: 'Progress by chapter',
                     description:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    'Lorem ipsum dolor sit amet',
                   ),
                 ),
                 Expanded(
-                  child: VideoBlock(
+                  child: DisciplineBlock(
                     image: 'assets/video.jpg',
-                    title: 'Videos included to improve your technique',
+                    ',
                     description:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    'Lorem ipsum dolor sit amet',
                   ),
                 ),
               ],
             ),
             SizedBox(height: 20.0),
-            ArticleBlock(
-              title: 'Article 1',
-              description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            ),
-            ArticleBlock(
-              title: 'Article 2',
-              description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            ),
-            SizedBox(height: 20.0),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.0),
-              child: Column(
-
-              ),
-            ),
           ],
         ),
       ),
@@ -110,86 +96,6 @@ class DisciplineBlock extends StatelessWidget {
       children: [
         Image.asset(image),
         SizedBox(height: 10.0),
-        Text(
-          title,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-        Text(description),
-        SizedBox(height: 20.0),
-      ],
-    );
-  }
-}
-
-class ProgressBlock extends StatelessWidget {
-  final String image;
-  final String title;
-  final String description;
-
-  const ProgressBlock({
-    required this.image,
-    required this.title,
-    required this.description,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(image),
-        SizedBox(height: 10.0),
-        Text(
-          title,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-        Text(description),
-        SizedBox(height: 20.0),
-      ],
-    );
-  }
-}
-
-class VideoBlock extends StatelessWidget {
-  final String image;
-  final String title;
-  final String description;
-
-  const VideoBlock({
-    required this.image,
-    required this.title,
-    required this.description,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(image),
-        SizedBox(height: 10.0),
-        Text(
-          title,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-        Text(description),
-        SizedBox(height: 20.0),
-      ],
-    );
-  }
-}
-
-class ArticleBlock extends StatelessWidget {
-  final String title;
-  final String description;
-
-  const ArticleBlock({
-    required this.title,
-    required this.description,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
         Text(
           title,
           style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
